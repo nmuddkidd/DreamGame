032626ab7debdb0c46e11af74fd1af635add4392
Kendall Rissler <kpriss01@louisville.edu>
2026-04-28 15:49:14 -0400

    Fixed Title Loading
    So there's another canvas in the player package. It is blocking the raycast from reaching the elements in the title screen. I added a check in the persist script to turn off the player game object and the canvas game object if it is the title screen.
    
    Persist loads up a check for each scene loaded that determines whether or not the other components should be disabled. Currently set to only check for title screen. Could also check for ending screens or any screen you don't want the interaction text and the player.

--- Assets/Scripts/persist.cs
+++ Assets/Scripts/persist.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class persist : MonoBehaviour
 {
@@ -6,8 +8,23 @@ public class persist : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        ApplyLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+        SceneManager.sceneLoaded += ApplyLoaded;
     }
 
+    private void ApplyLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "TitleScreen")
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            this.gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            this.gameObject.transform.GetChild(2).gameObject.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
