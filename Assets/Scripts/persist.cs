deb020ece82f9c9e6a18ec27f46b71f5ebb75b51
Kendall Rissler <kpriss01@louisville.edu>
2026-04-28 17:09:53 -0400

    Setting Spawn & Fixing Warnings
    There was an event system I forgot to remove from Griffins dream. That warning is now gone
    
    Anna had a player package that duplicated the character and gave bugs
    
    Persist now also changes the spawn location depending on the given scene.

--- Assets/Scripts/persist.cs
+++ Assets/Scripts/persist.cs
@@ -4,9 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class persist : MonoBehaviour
 {
+    private Vector3 spawner;
+    private GameObject player;
+    private GameObject interaction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = this.gameObject.transform.GetChild(0).gameObject;
+        interaction = this.gameObject.transform.GetChild(2).gameObject;
+
         DontDestroyOnLoad(gameObject);
         ApplyLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
         SceneManager.sceneLoaded += ApplyLoaded;
@@ -14,15 +20,39 @@ public class persist : MonoBehaviour
 
     private void ApplyLoaded(Scene scene, LoadSceneMode mode)
     {
+        //check for null
+        if (player == null)
+        {
+            Debug.LogWarning("Player object is null, cannot activate");
+            return;
+        }
+        else if (interaction == null)
+        {
+            Debug.LogWarning("Interaction screen object is null, cannot activate");
+            return;
+        }
+
+
         if (scene.name == "TitleScreen")
         {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            this.gameObject.transform.GetChild(2).gameObject.SetActive(false);
+            player.SetActive(false);
+            interaction.SetActive(false);
         }
         else
         {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-            this.gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            player.SetActive(true);
+            interaction.SetActive(true);
+        }
+        //set up unique spawners
+        if (scene.name == "AnnaDream")
+        {
+            spawner = new Vector3((float)72, (float)17, (float)93);
+            player.transform.localPosition = spawner;   
+        }
+        else if (scene.name == "SampleScene")
+        {
+            spawner = new Vector3((float)-16, (float)-0.4, (float)-7.5);
+            player.transform.localPosition = spawner;
         }
     }
     // Update is called once per frame
